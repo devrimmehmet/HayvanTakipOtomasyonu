7b238079f4e3daa74460b7f7e1191dced53c6c3e
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2018-10-16 04:29:40 +0300

    denem
    deneme

--- Hayvan Takip Otomasyonu/AnaMenu.Designer.cs
+++ Hayvan Takip Otomasyonu/AnaMenu.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -60,15 +61,24 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
-            // Form1
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(160, 233);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // AnaMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "AnaMenu";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -80,6 +90,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
