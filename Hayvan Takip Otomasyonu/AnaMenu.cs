f395e06d764bd934c286948194565cd12255b4d1
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2018-10-16 04:25:04 +0300

    rrrrrr
    rr

--- Hayvan Takip Otomasyonu/AnaMenu.cs
+++ Hayvan Takip Otomasyonu/AnaMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hayvan_Takip_Otomasyonu
 {
-    public partial class Form1 : Form
+    public partial class AnaMenu : Form
     {
-        public Form1()
+        public AnaMenu()
         {
             InitializeComponent();
         }
--- Hayvan Takip Otomasyonu/Program.cs
+++ Hayvan Takip Otomasyonu/Program.cs
@@ -16,7 +16,7 @@ namespace Hayvan_Takip_Otomasyonu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AnaMenu());
         }
     }
 }
